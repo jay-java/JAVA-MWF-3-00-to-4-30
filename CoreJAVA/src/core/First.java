9ae14453094dd3d2b84bc7b93a205801b338a439
jay-java <jayprajapati.tops@gmail.com>
2024-05-17 16:38:16 +0530

    first class

---
+++ CoreJAVA/src/core/First.java
@@ -0,0 +1,13 @@
+package core;
+
+//Three streams in java
+//1.in
+//2.out
+//3.err
+
+public class First {
+	public static void main(String[] jay) {
+		System.out.println("hello");
+		System.out.println("hello again");
+	}
+}
---
+++ CoreJAVA/src/core/Second.java
@@ -0,0 +1,14 @@
+package core;
+class abc{
+	public static void main(String[] args) {
+		System.out.println("abc class");
+	}
+}
+public class Second {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+	public static void main(Integer[] args) {
+		System.out.println("hello second main");
+	}
+}
